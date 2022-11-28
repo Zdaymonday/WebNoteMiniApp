9ecb523f629a82755964cc2e4ab8b739b6921e5e
Антон Королёв <iburninflame@gmail.com>
2022-11-28 16:09:45 +0300

    Add Models

---
+++ WebNoteMiniApp/Data/Note.cs
@@ -0,0 +1,10 @@
+﻿namespace WebNoteMiniApp.Data
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Created { get; set; }
+    }
+}
---
+++ WebNoteMiniApp/Data/NoteDb.cs
@@ -0,0 +1,8 @@
+﻿namespace WebNoteMiniApp.Data
+{
+    public class NoteDb : DbContext
+    {
+        public NoteDb(DbContextOptions<NoteDb> opt) : base(opt) { }
+        public DbSet<Note> Notes { get; set; } = null!;
+    }
+}

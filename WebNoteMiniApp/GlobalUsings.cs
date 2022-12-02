90fe5bf9c6cfad21325899eed718f7b60376f4a9
Антон Королёв <iburninflame@gmail.com>
2022-12-02 23:20:56 +0300

    Added vue project

--- WebNoteMiniApp/GlobalUsings.cs
+++ WebNoteMiniApp/GlobalUsings.cs
@@ -1,3 +1,7 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 global using WebNoteMiniApp.Data;
 global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.IdentityModel.Tokens;
+global using System.Text;
+global using WebNoteMiniApp.Auth;
